a183d10f9b2057186c154b2f3bba7f8947c9dbc3
Truong2003-tqd <xuantruongtqd@gmail.com>
2025-04-08 02:03:22 +0700

    Update 5. Relationship with Duration.R

--- 5. Relationship with Duration.R
+++ 5. Relationship with Duration.R
@@ -11,3 +11,37 @@ ggplot(df1, aes(x = duration_cat,y = credit_amount)) +
     axis.title = element_text(size = 10, color = "#0F4761"),
     axis.text = element_text(size = 9, color = "#0F4761"))
 }
+#Extract outliers for the upcoming analysis
+{
+  #Short-term outliers
+  short_term_outliers <- df1 %>% 
+    filter(duration_cat == "short term") %>%
+    pull(credit_amount) %>% 
+    boxplot.stats() %>% 
+    .$out
+  
+  short_term_outliers_rows <- which(df1$credit_amount %in% c(short_term_outliers))
+  
+  #Medium-term outliers
+  medium_term_outliers <- df1 %>% 
+    filter(duration_cat == "medium term") %>%
+    pull(credit_amount) %>% 
+    boxplot.stats() %>% 
+    .$out
+  
+  medium_term_outliers_rows <- which(df1$credit_amount %in% c(medium_term_outliers))
+  
+  #Long-term outliers
+  long_term_outliers <- df1 %>% 
+    filter(duration_cat == "long term") %>%
+    pull(credit_amount) %>% 
+    boxplot.stats() %>% 
+    .$out
+  
+  long_term_outliers_rows <- which(df1$credit_amount %in% c(long_term_outliers))
+  
+  
+  #Dataset outliers
+  outlier <- c(short_term_outliers,medium_term_outliers,long_term_outliers) 
+  outlier_rows <- which(df1$credit_amount %in% c(outlier))
+}
